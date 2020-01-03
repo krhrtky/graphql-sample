b2c01807b79cffacb057e2246b3611172a0c7d6c
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-03 22:03:29 +0900

    [add] postPhoto mutation.

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const typeDefs = `
   type Query {
     totalPhotos: Int!
   }
+  type Mutation {
+    postPhoto(name: String! description: String): Boolean!
+  }
   `;
 
+const photos = [];
 const resolvers = {
   Query: {
+    // 写真を格納した配列の長さを返す
     totalPhotos: () => 42,
+  },
+  // postPhotoミューテションと対応するリゾルバ
+  Mutation: {
+    postPhoto(parent, args) {
+      photos.push(args);
+      return true;
+    },
   },
 };
 
