a70e99b8e2e1feaf29a7a192af4a2f1ae5ef959a
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-03 22:30:57 +0900

    [add] return posted Obj & photo list query.

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  # 型定義
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo]!
   }
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
   `;
 
+// 一意なキー(ID)
+let _id = 0;
 const photos = [];
+
 const resolvers = {
   Query: {
     // 写真を格納した配列の長さを返す
-    totalPhotos: () => 42,
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
   // postPhotoミューテションと対応するリゾルバ
   Mutation: {
-    postPhoto(parent, args) {
-      photos.push(args);
-      return true;
+    postPhoto(_, args) {
+      // 新しい写真情報を作成
+      const newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+      // 作成した写真情報を返す
+      return newPhoto;
     },
   },
 };
