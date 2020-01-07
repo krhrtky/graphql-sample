a9379c945555793cf550e05cf2a2c8447951230d
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-08 00:40:49 +0900

    [add] scalar type: DateTime

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('apollo-server');
+const { GraphQLScalarType } = require('graphql');
 
 const typeDefs = `
+  scalar DateTime
   type User {
     """
     ユーザーの一位のGithHubログインID
@@ -37,6 +39,7 @@ const typeDefs = `
     category: PhotoCategory!
     postedBy: User!
     taggedUsers: [User!]!
+    created: DateTime!
   }
 
   input PostPhotoInput {
@@ -50,7 +53,7 @@ const typeDefs = `
 
   type Query {
     totalPhotos: Int!
-    allPhotos: [Photo]!
+    allPhotos(after: DateTime): [Photo!]!
   }
   type Mutation {
     postPhoto(input: PostPhotoInput!): Photo!
@@ -79,12 +82,14 @@ const photos = [
     description: 'The  heart chute is one of my favorite chutes',
     category: 'ACTION',
     githubUser: 'gPlake',
+    created: '3-28-1977',
   },
   {
     id: '2',
     name: 'Enjoying the sunshine',
     category: 'SELFIE',
     githubUser: 'sSchmidt',
+    created: '1-2-1985',
   },
   {
     id: '3',
@@ -92,6 +97,7 @@ const photos = [
     description: '25 laps on gunbarrel today',
     category: 'LANDSCAPE',
     githubUser: 'sSchmidt',
+    created: '2018-04-15T19:09:57.308Z',
   },
 ];
 
@@ -132,6 +138,13 @@ const resolvers = {
         .map(tag => tag.photoID)
         .map(photoID => photos.find(p => p.id === photoID)),
   },
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'A valid date time value',
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value,
+  }),
 };
 
 const server = new ApolloServer({
