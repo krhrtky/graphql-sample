a6de2fa3b0508eab6b0bb7011e26a3cdc3bba6a3
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-09 23:09:55 +0900

    [add] mongo client.

--- index.js
+++ index.js
@@ -2,66 +2,41 @@ const { ApolloServer } = require('apollo-server-express');
 const expressPlayground = require('graphql-playground-middleware-express')
   .default;
 const express = require('express');
-const { GraphQLScalarType } = require('graphql');
 const { readFileSync } = require('fs');
-
-const app = express();
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
-
-const users = [
-  { githubLogin: 'mHattup', name: 'Mike Hattrup' },
-  { githubLogin: 'gPlake', name: 'Glen Plake Hattrup' },
-  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
-];
-
-const tags = [
-  { photoID: '1', userID: 'gPlake' },
-  { photoID: '2', userID: 'sSchmidt' },
-  { photoID: '2', userID: 'mHattup' },
-  { photoID: '2', userID: 'gPlake' },
-];
-
-// 一意なキー(IDgPlake
-let _id = 0;
-const photos = [
-  {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The  heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-    created: '3-28-1977',
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-    created: '1-2-1985',
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-    created: '2018-04-15T19:09:57.308Z',
-  },
-];
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const start = async () => {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  console.log(MONGO_DB);
 
-server.applyMiddleware({ app });
-app.get('/', (_, res) => res.send('Welcome to the PhotoShare API'));
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
-
-app.listen({ port: 4000 }, () => {
-  console.log(
-    `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+  const client = await MongoClient.connect(
+    MONGO_DB,
+    { useNewUrlParser: true }
   );
-});
+
+  const db = client.db();
+  const context = { db };
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+  });
+
+  server.applyMiddleware({ app });
+
+  app.get('/', (_, res) => res.send('Welcome to the PhotoShare API'));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  app.listen({ port: 4000 }, () => {
+    console.log(
+      `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    );
+  });
+};
+
+start();
